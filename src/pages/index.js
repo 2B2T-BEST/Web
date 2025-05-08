742ec53f8980f3817457c249a86df2ccc896172b
HiTech0926 <2025138508@qq.com>
2025-05-08 22:32:56 +0800

    change: text and link

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ function HomepageHeader() {
             <Link
               className={clsx('button', styles.primaryButton)}
               to="/docs/intro">
-              文档 🚀
+              文档
             </Link>
           </div>
         </div>
@@ -56,16 +56,16 @@ function HomeFooterCTA() {
     <section className={styles.footerCta}>
       <div className="container">
         <Heading as="h2" className={styles.footerCtaTitle}>
-          准备好开始你的 <span className={styles.textHighlight}>Minecraft</span> 服务器旅程了吗？
+          准备好开始你的 <span className={styles.textHighlight}>2B2T.BEST</span> 无规则服务器旅程了吗？
         </Heading>
         <p className={styles.footerCtaSubtitle}>
-          我们提供从零开始的详细教程，帮助你轻松搭建和管理自己的服务器。无论你是新手还是有经验的玩家，这里都有适合你的内容。
+          无规则MC的终极答案，性能与混沌的完美平衡！让我们开始吧！
         </p>
         <div>
           <Link
             className={clsx('button button--primary button--lg')}
-            to="/">
-            立即开始
+            href="https://qm.qq.com/q/qnCs3wThNQs">
+            立即游玩
           </Link>
         </div>
       </div>
